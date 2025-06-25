40f4b827c8a5c8d7bb72d48c29c519e09ae63b26
Alif1507 <m.alifwahyudi2007@gmail.com>
2025-06-25 23:03:55 +0700

    test dploy

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# Dockerfile
+FROM php:8.3-fpm
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpng-dev \
+    libjpeg-dev \
+    libonig-dev \
+    libxml2-dev \
+    zip \
+    unzip \
+    curl \
+    git \
+    npm \
+    nodejs
+
+# Install PHP extensions
+RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd
+
+# Set working directory
+WORKDIR /var/www
+
+# Copy existing application
+COPY . .
+
+# Install Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+RUN composer install
+
+# Set permissions
+RUN chown -R www-data:www-data /var/www \
+    && chmod -R 755 /var/www
+
+EXPOSE 9000
+CMD ["php-fpm"]
--- resources/views/components/projek.blade.php
+++ resources/views/components/projek.blade.php
@@ -5,11 +5,23 @@
         <h1 class="font-inter font-bold text-4xl max-md:text-2xl text-white">Proyek Jurnalistik 26</h1>
         <div class="bg-[#F09B0E] w-[272px] h-1">.</div>
       </div>
-
-      <div class="flex items-center justify-evenly mt-10 max-md:flex-col max-md:gap-10">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/OXorW3jw0QE?si=_-lH44eiJvORAvnU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
-        <img class="w-[520px] h-auto" src="{{ asset('images/fl2sn 2.png') }}" alt="">
+      <div class="flex items-center justify-evenly mt-10 max-md:flex-col max-md:gap-6">
+        <div class="w-[320px] max-w-full aspect-video">
+          <iframe 
+        class="w-full h-full"
+        src="https://www.youtube.com/embed/OXorW3jw0QE?si=_-lH44eiJvORAvnU" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+          </iframe>
+        </div>
+        <img 
+          class="w-[320px] max-w-full h-auto" 
+          src="{{ asset('images/fl2sn 2.png') }}" 
+          alt=""
+        >
+      </div>
       </div>
 </div>
